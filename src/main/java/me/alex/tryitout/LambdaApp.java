bcbb43c033f4b21bfad08f2392d03ede9dcd388c
Alex Poiry <alex.poiry@cerner.com>
2019-01-02 14:32:53 -0600

    AP-1 Initial Commit
    Ran Maven archetype, updated git data, added some basic lambda
    code.

---
+++ src/main/java/me/alex/tryitout/LambdaApp.java
@@ -0,0 +1,66 @@
+package me.alex.tryitout;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.concurrent.Executors;
+
+/**
+ * Hello Lambda!
+ *
+ */
+public class LambdaApp
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello Lambda!" );
+
+        // Code is written in an anonymous class
+        // Code is passed to other code to run later
+        // Could be run in another context(thread)
+        // Effectively passed code as a parameter
+        Runnable r1 = new Runnable() {
+            @Override
+            public void run() {
+                Comparator<String> comparator = new Comparator<String>() {
+                    @Override
+                    public int compare(String o1, String o2) {
+                        return Integer.compare(o1.length(), o2.length());
+                    }
+                };
+
+                String[] testString = {"ccc", "a", "bb"};
+
+                Arrays.sort(testString, comparator);
+
+                for (String test : testString) {
+                    System.out.println(test);
+                }
+
+                return;
+            }
+        };
+
+        Executors.newSingleThreadExecutor().execute(r1);
+
+        // So, when a class implements a single, abstract method, you can make it a lambda.
+        // Just name your object, add an =, put the necessary args, if any, in the parens,
+        // draw a right arrow, and implement your code.
+        Runnable r2 = () -> {
+            //Comparator<String> comparator = (o1, o2) -> Integer.compare(o1.length(), o2.length());
+            Comparator<String> comparator = Comparator.comparingInt(String::length);
+
+            String[] testString = {"fff", "d", "ee"};
+
+            Arrays.sort(testString, comparator);
+
+            for (String test : testString) {
+                System.out.println(test);
+            }
+
+            System.exit(0);
+        };
+
+        Executors.newSingleThreadExecutor().execute(r2);
+
+    }
+}
---
+++ src/test/java/me/alex/tryitout/LambdaAppTest.java
@@ -0,0 +1,20 @@
+package me.alex.tryitout;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple LambdaApp.
+ */
+public class LambdaAppTest
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
